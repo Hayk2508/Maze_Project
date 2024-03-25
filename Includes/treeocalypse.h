97803be4c65df0b7a39a55b55fb3262a1fded5c0
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-03-25 12:50:15 +0400

    testing planting()

--- Includes/treeocalypse.h
+++ Includes/treeocalypse.h
@@ -8,8 +8,10 @@ public:
     Treeocalypse();
     void planting();
     void setDistancesFromPlayer(humanPlayer& p);
+    void findEmptyCells();
 private:
     bool isWinnable();
     std::vector<std::vector<int>> distances;
     std::vector<Tree> plantedTrees;
+    std::vector<std::pair<int,int>> emptyCells;
 };
