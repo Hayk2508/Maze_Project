35c320c82f82c2627637a0e33477bfed10590197
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2024-03-26 12:46:47 +0400

    First mode almost finished

--- Includes/labyrinth.h
+++ Includes/labyrinth.h
@@ -1,24 +1,33 @@
 #pragma once
+
 #include "game.h"
 #include <vector>
 #include <iostream>
 
-class Labyrinth:public Game{
+class Labyrinth : public Game {
 public:
-    static bool isValidCell(int x,int y);
-    void generateGrid(int x,int y);
+    static bool isValidCell(int x, int y);
+
+    void generateGrid(int x, int y);
+
     void play() override;
+
     Labyrinth();
+
 protected:
     static void launchWelcomeToTheJungle();
+
     static bool isBorderCell(int x, int y);
+
     [[noreturn]]  void launchTreeocalypse();
+
     void draw() override;
+
     void generateExits(int x, int y);
 
-    std::vector<std::pair<int,int>> exits;
-   static  constexpr int  mazeWidth = 20;
-   static  constexpr int  mazeHeight = 20;
-   std::vector<std::vector<char>> maze;
-   std::vector<std::pair<int,int>> winnablePath;
+    std::vector<std::pair<int, int>> exits;
+    static constexpr int mazeWidth = 25;
+    static constexpr int mazeHeight = 25;
+    std::vector<std::vector<char>> maze;
+    std::vector<std::pair<int, int>> winnablePath;
 };
